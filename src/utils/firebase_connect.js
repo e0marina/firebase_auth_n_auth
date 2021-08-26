426d127d016640dc1be31664417723da97925a86
e0marina <emilymarinaro@gmail.com>
2021-08-25 21:02:45 -0700

    added initial files and env

---
+++ src/utils/firebase_connect.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import env from 'react-dotenv';
+
+// TODO: Replace the following with your app's Firebase project configuration
+
+const firebaseConfig = {
+  apiKey: env.API_KEY,
+  authDomain: env.AUTH_DOMAIN,
+  projectId: env.PROJECT_ID,
+  storageBucket: env.STORAGE_BUCKET,
+  messagingSenderId: env.MESSAGING_SENDER_ID,
+  appId: env.APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
